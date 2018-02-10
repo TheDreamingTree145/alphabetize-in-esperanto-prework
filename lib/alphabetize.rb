e6947aa0a21824f3783ea53e06e00c5911bf641f
Dustin Anderson <dandersonsoftware@outlook.com>
2018-02-10 01:55:00 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,6 +3,6 @@ ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by{|phrase|
-    phrase.split("").map{|letter|alphabet.index(letter)}
+    phrase.split("").map{|letter|alphabet.index(letter)}}
   end
 end
